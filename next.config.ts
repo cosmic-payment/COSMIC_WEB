c30ce331b9601d1b95c9beb27d45bb25714c9bfb
Emmanuel Mnanka Samo <emmanuelmunanka38@gmail.com>
2026-08-10 07:03:05 +0300

    chore(config): allow remote image domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
+      {
+        protocol: "https",
+        hostname: "www.untitledui.com",
+      },
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
